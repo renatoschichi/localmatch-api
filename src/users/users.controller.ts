ee453128f28f1bf6f63449befffdbdc91e303da2
Renato Schichi <renatoschichi@gmail.com>
2025-04-14 21:00:56 -0300

    removing unecessary code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ApiTags, ApiBearerAuth, ApiCreatedResponse, ApiBody } from '@nestjs/swagger';
-import { AdminCreateUserDto } from './admin-create-user.dto';
 import { AdminUpdateUserDto } from './admin-update-user.dto';
-import { AdminOnlyGuard } from 'src/auth/guards/admin-only.guard';
-import { SelfOrAdminGuard } from 'src/auth/guards/self-or-admin.guard';
-import { AdminCreationGuard } from 'src/auth/guards/admin-creation.guard';
-import { CreateUserDto } from './create-user.dto';
 
 @ApiTags('users')
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
-  @Post()
-  @ApiBody({ type: AdminCreateUserDto })
-  @ApiCreatedResponse({ type: User, description: 'Usuário criado com sucesso' })
-  create(@Body() adminCreateUserDto: AdminCreateUserDto): Promise<User> {
-    return this.usersService.create(adminCreateUserDto);
-  }
-
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   findAll(@Query('role') role?: string): Promise<User[]> {
